7a83693c872e20df3051fefbd37f386581363405
l_momowj <l_momowj@163.com>
2024-03-16 13:17:39 +0800

    update chap3

--- chap3/nnc/dataset.py
+++ chap3/nnc/dataset.py
@@ -9,8 +9,6 @@ Moon Data  ---   Classification
 """
 
 import os
-import torch
-from torch.utils import data
 import math
 import random
 import numpy as np
@@ -46,11 +44,11 @@ def seed_random(seed=1029):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed) # 为了禁止hash随机化，使得实验可复现
     np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True    
+    #torch.manual_seed(seed)
+    #torch.cuda.manual_seed(seed)
+    #torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    #torch.backends.cudnn.benchmark = False
+    #torch.backends.cudnn.deterministic = True    
 
 # (2) 生成数据集
 # 数据类
@@ -69,24 +67,22 @@ def gen_synthetic_data(func,interval,sample_num,noise,add_outlier=False,outlier_
         - X: 特征数据，shape=[n_samples,1]
         - y: 标签数据，shape=[n_samples,1]
     # """
-    
-    # print('sample_num:',sample_num)
 
     # 均匀采样
-    # 使用 torch.rand 在生成 sample_num 个随机数    
-    X = torch.rand(sample_num) * (interval[1]-interval[0]) + interval[0]
+    # 使用 np.random.rand 在生成 sample_num 个随机数    
+    X = np.random.rand(sample_num) * (interval[1]-interval[0]) + interval[0]
     y = func(X)
 
     # 生成高斯分布的标签噪声
-    # torch.normal生成0均值，noise标准差的数据
-    epsilon = torch.normal(0, noise, y.shape)
+    # np.random.normal生成0均值，noise标准差的数据
+    epsilon = np.random.normal(0, noise, y.shape)
     y = y + epsilon
     if add_outlier:     # 生成额外的异常点
         outlier_num = int(len(y)*outlier_ratio)
         if outlier_num != 0:
             # 使用torch.randint生成服从均匀分布的、范围在[0, len(y))的随机Tensor
-            outlier_idx = torch.randint(len(y),shape = [outlier_num])
-            y[outlier_idx] = y[outlier_idx] * 5            
+            outlier_idx = np.random.randint(len(y),size = [outlier_num])
+            y[outlier_idx] = y[outlier_idx] * 5    
     return X, y
 
 class sin_data():
@@ -100,7 +96,7 @@ class sin_data():
         
     def sin(self,x):
         # sin 函数: sin(2*pi*x)
-        return torch.sin(2*np.pi*x)
+        return np.sin(2*np.pi*x)
 
     def gen_data(self,train_num,**kwargs):        
         # 由 sin(2*pi*x) 生成带噪声的数据
@@ -116,7 +112,7 @@ class sin_data():
         self.test_data = (X_test,y_test)
         
         # 由函数生成的绘图数据的曲线
-        self.X_underlying = torch.linspace(interval[0],interval[1],steps=100) 
+        self.X_underlying = np.linspace(interval[0],interval[1],100) 
         self.y_underlying = self.sin(self.X_underlying)  
 
     # 绘制散点图
@@ -128,32 +124,32 @@ class sin_data():
         # 训练数据
         X_train,y_train = kwargs.get('train_data',(None,None))
         if X_train is not None and y_train is not None:
-            if not isinstance(X_train ,torch.Tensor): X_train = torch.Tensor(X_train)
-            if not isinstance(y_train ,torch.Tensor): y_train = torch.Tensor(y_train) 
+            if not isinstance(X_train ,np.ndarray): X_train = np.array(X_train)
+            if not isinstance(y_train ,np.ndarray): y_train = np.array(y_train) 
 
         # 测试数据
         X_test,y_test = kwargs.get('test_data',(None,None))
         if X_test is not None and y_test is not None:
-            if not isinstance(X_test ,torch.Tensor): X_test = torch.Tensor(X_test)
-            if not isinstance(y_test ,torch.Tensor): y_test = torch.Tensor(y_test) 
+            if not isinstance(X_test ,np.ndarray): X_test = np.array(X_test)
+            if not isinstance(y_test ,np.ndarray): y_test = np.array(y_test) 
 
         #　由函数生成的绘图数据的曲线
         X_underlying,y_underlying = kwargs.get('underlying_data',(self.X_underlying,self.y_underlying))
         if X_underlying is not None and y_underlying is not None:
-            if not isinstance(X_underlying ,torch.Tensor): X_underlying = torch.Tensor(X_underlying)
-            if not isinstance(y_underlying ,torch.Tensor): y_underlying = torch.Tensor(y_underlying)    
+            if not isinstance(X_underlying ,np.ndarray): X_underlying = np.array(X_underlying)
+            if not isinstance(y_underlying ,np.ndarray): y_underlying = np.array(y_underlying)    
         
         # 由模型生成的绘图数据的预测曲线
         X_underlying_pred,y_underlying_pred = kwargs.get('pred_underlying_data',(None,None))
         if X_underlying_pred is not None and y_underlying_pred is not None:
-            if not isinstance(X_underlying_pred ,torch.Tensor): X_underlying_pred = torch.Tensor(X_underlying_pred)
-            if not isinstance(y_underlying_pred ,torch.Tensor): y_underlying_pred = torch.Tensor(y_underlying_pred) 
+            if not isinstance(X_underlying_pred ,np.ndarray): X_underlying_pred = np.array(X_underlying_pred)
+            if not isinstance(y_underlying_pred ,np.ndarray): y_underlying_pred = np.array(y_underlying_pred) 
 
         # 由正则化模型生成的绘图数据的预测曲线
         X_underlying_pred_reg,y_underlying_pred_reg = kwargs.get('reg_pred_underlying_data',(None,None))
         if X_underlying_pred_reg is not None and y_underlying_pred_reg is not None:
-            if not isinstance(X_underlying_pred_reg ,torch.Tensor): X_underlying_pred_reg = torch.Tensor(X_underlying_pred_reg)
-            if not isinstance(y_underlying_pred_reg ,torch.Tensor): y_underlying_pred_reg = torch.Tensor(y_underlying_pred_reg) 
+            if not isinstance(X_underlying_pred_reg ,np.ndarray): X_underlying_pred_reg = np.array(X_underlying_pred_reg)
+            if not isinstance(y_underlying_pred_reg ,np.ndarray): y_underlying_pred_reg = np.array(y_underlying_pred_reg) 
         
         label = kwargs.get('label','')
         label_pred = label
@@ -165,7 +161,7 @@ class sin_data():
         if reg_lambda != 0:
             label_pred_reg = r"$M={},\ell_2\ reg\ (\lambda={})$".format(degree,reg_lambda)
         
-        if (X_train,y_train) == (None,None) and (X_test,y_test) == (None,None) and (X_underlying,y_underlying) == (None,None):
+        if (X_train is None and y_train is None) and (X_test is None and y_test is None) and (X_underlying is None and y_underlying is None):
             return 
         
         plt.rcParams['figure.figsize'] = (7.0,5.0)
@@ -223,47 +219,44 @@ class moons_data():
         n_samples_in = n_samples - n_samples_out
 
         # 采集第1类数据，特征为(x,y)
-        # 使用'torch.linspace'在0到pi上均匀取n_samples_out个值
-        # 使用'torch.cos'计算上述取值的余弦值作为特征1，使用'torch.sin'计算上述取值的正弦值作为特征2
-        outer_circ_x = torch.cos(torch.linspace(0, math.pi, n_samples_out))
-        outer_circ_y = torch.sin(torch.linspace(0, math.pi, n_samples_out))
+        # 使用'np.linspace'在0到pi上均匀取n_samples_out个值
+        # 使用'np.cos'计算上述取值的余弦值作为特征1，使用'np.sin'计算上述取值的正弦值作为特征2
+        outer_circ_x = np.cos(np.linspace(0, math.pi, n_samples_out))
+        outer_circ_y = np.sin(np.linspace(0, math.pi, n_samples_out))
 
-        inner_circ_x = 1 - torch.cos(torch.linspace(0, math.pi, n_samples_in))
-        inner_circ_y = 0.5 - torch.sin(torch.linspace(0, math.pi, n_samples_in))
+        inner_circ_x = 1 - np.cos(np.linspace(0, math.pi, n_samples_in))
+        inner_circ_y = 0.5 - np.sin(np.linspace(0, math.pi, n_samples_in))
 
         #print('outer_circ_x.shape:', outer_circ_x.shape, 'outer_circ_y.shape:', outer_circ_y.shape)
         #print('inner_circ_x.shape:', inner_circ_x.shape, 'inner_circ_y.shape:', inner_circ_y.shape)
 
-        # 使用'torch.concat'将两类数据的特征1和特征2分别延维度0拼接在一起，得到全部特征1和特征2
-        # 使用'torch.stack'将两类特征延维度1堆叠在一起
-        X = torch.stack(
-            [torch.concat([outer_circ_x, inner_circ_x]),
-            torch.concat([outer_circ_y, inner_circ_y])],
+        # 使用'np.concatenate'将两类数据的特征1和特征2分别延维度0拼接在一起，得到全部特征1和特征2
+        # 使用'np.stack'将两类特征延维度1堆叠在一起
+        X = np.stack(
+            [np.concatenate([outer_circ_x, inner_circ_x]),
+            np.concatenate([outer_circ_y, inner_circ_y])],
             axis=1
         )
 
-        #print('after concat shape:', torch.concat([outer_circ_x, inner_circ_x]).shape)
-        #print('X shape:', X.shape)
-
-        # 使用'torch. zeros'将第一类数据的标签全部设置为0
+        # 使用'np. zeros'将第一类数据的标签全部设置为0
         # 使用'torch. ones'将第一类数据的标签全部设置为1
-        y = torch.concat(
-            [torch.zeros(size=[n_samples_out],dtype=int), torch.ones(size=[n_samples_in],dtype=int)]
+        y = np.concatenate(
+            [np.zeros(shape=[n_samples_out],dtype=int), np.ones(shape=[n_samples_in],dtype=int)]
         )
 
         #print('y shape:', y.shape)
 
         # 如果shuffle为True，将所有数据打乱
         if shuffle:
-            # 使用'torch.randperm'生成一个数值在0到X.shape[0]，随机排列的一维Tensor做索引值，用于打乱数据
-            idx = torch.randperm(X.shape[0])
+            # 使用'np.random.permutation'生成一个数值在0到X.shape[0]，随机排列的一维array做索引值，用于打乱数据
+            idx = np.random.permutation(X.shape[0])
             X = X[idx]
             y = y[idx]
 
         # 如果noise不为None，则给特征值加入噪声
         if noise is not None:
             # 使用'torch.normal'生成符合正态分布的随机Tensor作为噪声，并加到原始特征上
-            X += torch.normal(mean=0.0, std=noise, size=X.shape)
+            X += np.random.normal(loc=0.0, scale=noise, size=X.shape)
         
         self.all_data = (X,y)
         # 构建训练集、验证集和测试集
@@ -285,8 +278,6 @@ class moons_data():
             draw_classification_data(fig_path, fig_name,self.test_data)
         elif data_type == 'dev':
             draw_classification_data(fig_path, fig_name,self.dev_data)
-        
-
 
 def gen_multiclass_data(n_samples=100, n_features=2, n_classes=3, shuffle=True, noise=0.1):
     """
@@ -305,16 +296,16 @@ def gen_multiclass_data(n_samples=100, n_features=2, n_classes=3, shuffle=True, 
     for i in range(n_samples - sum(n_samples_per_class)):
         n_samples_per_class[i % n_classes] += 1
     # 将特征和标签初始化为0
-    X = torch.zeros([n_samples, n_features])
-    y = torch.zeros([n_samples],dtype=torch.int64)
+    X = np.zeros([n_samples, n_features])
+    y = np.zeros([n_samples],dtype=np.int64)
     # 随机生成3个簇中心作为类别中心
-    centroids = torch.randperm(2 ** n_features)[:n_classes]
-    centroids_bin = np.unpackbits(centroids.numpy().astype('uint8')).reshape((-1, 8))[:, -n_features:]
-    centroids = torch.tensor(centroids_bin).float()
+    centroids = np.random.permutation(2 ** n_features)[:n_classes]
+    centroids_bin = np.unpackbits(centroids.astype('uint8')).reshape((-1, 8))[:, -n_features:]
+    centroids = np.array(centroids_bin).astype(np.float64)
     # 控制簇中心的分离程度
     centroids = 1.5 * centroids - 1
     # 随机生成特征值
-    X[:, :n_features] = torch.randn(size=[n_samples, n_features])
+    X[:, :n_features] = np.random.randn(n_samples, n_features)
 
     stop = 0
     # 将每个类的特征值控制在簇中心附近
@@ -324,22 +315,22 @@ def gen_multiclass_data(n_samples=100, n_features=2, n_classes=3, shuffle=True, 
         y[start:stop] = k % n_classes
         X_k = X[start:stop, :n_features]
         # 控制每个类别特征值的分散程度
-        A = 2 * torch.rand(size=[n_features, n_features]) - 1
-        X_k[...] = torch.matmul(X_k, A)
+        A = 2 * np.random.rand(n_features, n_features) - 1
+        X_k[...] = np.matmul(X_k, A)
         X_k += centroid
         X[start:stop, :n_features] = X_k
 
     # 如果noise不为None，则给特征加入噪声
     if noise > 0.0:
         # 生成noise掩膜，用来指定给那些样本加入噪声
-        noise_mask = torch.rand([n_samples]) < noise
+        noise_mask = np.random.rand(n_samples) < noise
         for i in range(len(noise_mask)):
             if noise_mask[i]:
                 # 给加噪声的样本随机赋标签值
-                y[i] = torch.randint(n_classes, size=[1],dtype=torch.int64)
+                y[i] = np.random.randint(n_classes, size=[1],dtype=np.int64)
     # 如果shuffle为True，将所有数据打乱
     if shuffle:
-        idx = torch.randperm(X.shape[0])
+        idx = np.random.permutation(X.shape[0])
         X = X[idx]
         y = y[idx]
 
@@ -348,12 +339,12 @@ def gen_multiclass_data(n_samples=100, n_features=2, n_classes=3, shuffle=True, 
 
 def gen_timeseries_data(func,num_samples,tau,normal_params=(0,0.1)):    
     # T = 1000 # 总共产⽣1000个点
-    time = torch.arange(1, num_samples+1, dtype=torch.float32)
+    time = np.arange(1, num_samples+1, dtype=np.float32)
     mu,std = normal_params
-    x = func(0.01*time) + torch.normal(mu,std,(num_samples,))
+    x = func(0.01*time) + np.random.normal(mu,std,(num_samples,))
     plot(time,[x], 'time','x',xlim=[1,num_samples],figsize=(6,3))
     
-    features = torch.zeros((num_samples-tau, tau))
+    features = np.zeros((num_samples-tau, tau))
     for i in range(tau):
         features[:,i] = x[i:num_samples-tau+i]
     labels = x[tau:].reshape((-1,1))
@@ -361,11 +352,11 @@ def gen_timeseries_data(func,num_samples,tau,normal_params=(0,0.1)):
     return features,labels
 
 
-def load_array(data_arrays, batch_size, is_train=True):
-    """Construct a PyTorch data iterator.
-    Defined in :numref:`sec_linear_concise`"""
-    dataset = data.TensorDataset(*data_arrays)
-    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+#def load_array(data_arrays, batch_size, is_train=True):
+#    #"""Construct a PyTorch data iterator.
+#    #Defined in :numref:`sec_linear_concise`"""
+#    dataset = data.TensorDataset(*data_arrays)
+#    return data.DataLoader(dataset, batch_size, shuffle=is_train)
 
 def data_split(X,y,train_percent=0.8,**kwargs): # test_percent=0.2,dev_percent=0):    
     # 划分训练集、测试集、验证集   
@@ -375,7 +366,7 @@ def data_split(X,y,train_percent=0.8,**kwargs): # test_percent=0.2,dev_percent=0
     assert train_percent+test_percent+dev_percent == 1  # 数据集占比和为1 
     
     n = len(X)
-    shuffled_indices = torch.randperm(n) # 返回一个数值在0到n-1、随机排列的1-D Tensor
+    shuffled_indices = np.random.permutation(n) # 返回一个数值在0到n-1、随机排列的1-D Tensor
     train_set_size = int(n*train_percent) 
     train_indices = shuffled_indices[:train_set_size]
     if dev_percent == 0:        
@@ -389,7 +380,7 @@ def data_split(X,y,train_percent=0.8,**kwargs): # test_percent=0.2,dev_percent=0
     # X = X.values
     # y = y.values
     
-    y = y.float()
+    y = y.astype(np.float64)
     X_train = X[train_indices]
     y_train = y[train_indices].reshape([-1,1])
     
